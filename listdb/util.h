b5dea5d9fbbed6896f1631d2542246ad61f8aef4
Wonbae Kim <wbkim@unist.ac.kr>
2022-04-21 12:49:51 +0000

    bug fix: string key comparator

--- listdb/util.h
+++ listdb/util.h
@@ -8,6 +8,19 @@ inline size_t AlignedSize(const size_t align, const size_t size) {
   return (mod == 0) ? size : size + (align - mod);
 }
 
+inline uint64_t KeyNum(const unsigned char* buf) {
+  uint64_t number;
+  number = static_cast<uint64_t>(buf[0]) << 56
+        | static_cast<uint64_t>(buf[1]) << 48
+        | static_cast<uint64_t>(buf[2]) << 40
+        | static_cast<uint64_t>(buf[3]) << 32
+        | static_cast<uint64_t>(buf[4]) << 24
+        | static_cast<uint64_t>(buf[5]) << 16
+        | static_cast<uint64_t>(buf[6]) << 8
+        | static_cast<uint64_t>(buf[7]);
+  return number;
+}
+
 }  // namespace util
 
 #endif  // LISTDB_UTIL_H_
