9798a794e3deda869e34db30cb58d3a97584ca76
--add <--add>
2023-12-18 06:13:25 +0000

    backup before apply bloom filter

--- listdb/ubench/rw_ratio.cc
+++ listdb/ubench/rw_ratio.cc
@@ -32,10 +32,10 @@
 //#define QUERY_DISTRIBUTION "zipf"
 
 constexpr int NUM_THREADS = 60;
-constexpr size_t NUM_LOADS = 10 * 1000 * 1000;
-constexpr size_t NUM_WORKS = 10 * 1000 * 1000;
+constexpr size_t NUM_LOADS = 100 * 1000 * 1000;
+constexpr size_t NUM_WORKS = 100 * 1000 * 1000;
 
-constexpr int SLEEP_TIME = 20;//time to waiting l0 compactions end
+constexpr int SLEEP_TIME = 300;//time to waiting l0 compactions end
 constexpr int SLEEP_TIME2 = 20;//time to waiting l1 compactions end
 constexpr int READ_RATIO = 100;//set 200 to do scan
 
@@ -140,7 +140,7 @@ void FillWorkKeys(const size_t num_works, std::vector<OpType>* work_ops,
 
 void FillLoadKeysReadRatio(const size_t num_loads, const size_t num_works, std::vector<uint64_t>* load_keys, unsigned int read_ratio) {
   std::stringstream ss;
-  ss << "/juwon/index-microbench/workloads_rw_ratio_unif/"; //test juwon
+  ss << "/juwon/index-microbench/ycsb_workloadc/"; //test juwon
   ss << "load_r" << read_ratio << "_unif_int_" << (num_loads / 1000 / 1000) << "M_" << (num_works / 1000 / 1000) << "M";
   FillLoadKeys(num_loads, load_keys, ss.str());
 }
@@ -148,7 +148,7 @@ void FillLoadKeysReadRatio(const size_t num_loads, const size_t num_works, std::
 void FillWorkKeysReadRatio(const size_t num_loads, const size_t num_works, std::vector<OpType>* work_ops,
                            std::vector<uint64_t>* work_keys, std::vector<uint64_t>* work_scan_nums, unsigned int read_ratio) {
   std::stringstream ss;
-  ss << "/juwon/index-microbench/workloads_rw_ratio_unif/"; //test juwon
+  ss << "/juwon/index-microbench/ycsb_workloadc/"; //test juwon
   ss << "run_r" << read_ratio << "_unif_int_" << (num_loads / 1000 / 1000) << "M_" << (num_works / 1000 / 1000) << "M";
   FillWorkKeys(num_works, work_ops, work_keys, work_scan_nums, ss.str());
 }
